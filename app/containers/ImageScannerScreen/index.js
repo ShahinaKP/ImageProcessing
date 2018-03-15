f2d04740587abeb8c34fe0b3ee86fbfe5e4feb33
Shahina <shahina@qburst.com>
2018-03-15 10:20:18 +0530

    Extract the name and licence number from a predefined format

--- app/containers/ImageScannerScreen/index.js
+++ app/containers/ImageScannerScreen/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import {
+    ActivityIndicator,
     Text,
     Image,
     View,
-    Alert
+    Alert,
+    Dimensions
 } from 'react-native';
 
 
@@ -13,22 +15,25 @@ import RNTesseractOcr from 'react-native-tesseract-ocr';
 
 const tessOptions = {
     whitelist: null,
-    blacklist: '1234567890\'!"#$%&/()={}[]+*-_:;<>'
+    blacklist: '!"#$%&/()={}[]+*-_:;<>'
 };
 
 export default class ImageScannerScreen extends Component {
     constructor(props) {
         super(props);
-        this.state = { isLoading: false,
-                        ocrResult: null 
-                     };
+        this.state = {
+            isLoading: false,
+            ocrResultName: '',
+            ocrResultNum: '',
+            screenWidth: Dimensions.get('window').width
+        };
     }
 
     doOcr(path) {
         RNTesseractOcr.recognize(path, 'LANG_ENGLISH', tessOptions)
             .then((result) => {
-                this.setState({ ocrResult: result });
-                // console.log('OCR Result: ', result);
+                var str1 = `Name: Shahina\n Licence No:1212121212`;
+                this.getNameAndNum(result);                
             })
             .catch((err) => {
                 console.log('OCR Error: ', err);
@@ -36,11 +41,43 @@ export default class ImageScannerScreen extends Component {
             .done();
     }
 
+    getNameAndNum = (data) => {
+        let dataLines = data.split('\n');
+        let nameArr = this.searchStringInArray('Name',dataLines)
+        let numArr = this.searchStringInArray('Licence No',dataLines)
+
+        if(nameArr !== -1) {
+            let nameArrStrs = nameArr.split('Name');
+            this.setState({ ocrResultName: 'Name: '+nameArrStrs[1].slice(2)});
+        }
+        else {
+            this.setState({ ocrResultName: 'No data available',isLoading: false});
+        }
+        
+        if(numArr !== -1) {
+            let numArrStrs = numArr.split('Licence No');
+            this.setState({ ocrResultNum: 'Licence No: '+numArrStrs[1].slice(2)},
+                            () =>{
+                                this.setState({isLoading: false})
+                        });
+        }
+        else {
+            this.setState({ ocrResultNum: '',isLoading: false});
+        }
+    }
+
+    searchStringInArray = (str, strArray) => {
+        for (var j=0; j<strArray.length; j++) {
+            if (strArray[j].match(str)) return strArray[j];
+        }
+        return -1;
+    }
+
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={{ color: '#ccc' }}>ImageScannerScreen</Text>
+                <Text style={styles.headerText}>Upload your image here and get the details</Text>
                 <PhotoUpload
                     onPhotoSelect={avatar => {
                         if (avatar) {
@@ -50,25 +87,35 @@ export default class ImageScannerScreen extends Component {
 
                     onResponse={response => {
                         if (response) {
+                            this.setState({isLoading: true});
                             this.doOcr(response.path);
                         }
                     }}
+                    
+                    onCancel= {event => {
+                        this.setState({isLoading: false});
+                    }}
                 >
                     <Image
-                        style={{
-                            paddingVertical: 30,
-                            width: 150,
-                            height: 150,
-                            borderRadius: 75
-                        }}
-                        resizeMode='cover'
-                        source={{
-                            uri: 'https://www.sparklabs.com/forum/styles/comboot/theme/images/default_avatar.jpg'
-                        }}
+                        style={[{width: this.state.screenWidth},styles.imageHolder]}                       
+                        resizeMode='contain'
+                        source={require('../../images/img-placeholder.png')}
                     />
                 </PhotoUpload>
 
-                <Text style={{ color: '#ccc' }}>{this.state.ocrResult}</Text>
+                {(this.state.isLoading) ?
+                    <ActivityIndicator
+                        animating={this.state.isLoading}
+                        color = '#ccc'
+                        size="large"
+                        style = {styles.activityIndicator}
+                    />
+                    :
+                    null
+                }
+
+                <Text style={styles.imageData}>{this.state.ocrResultName}</Text>
+                <Text style={styles.imageData}>{this.state.ocrResultNum}</Text>
 
             </View>
         );
--- app/containers/ImageScannerScreen/style.js
+++ app/containers/ImageScannerScreen/style.js
@@ -4,7 +4,28 @@ export default StyleSheet.create({
   container: {
       flex: 1,
       backgroundColor: '#111',
-      alignItems: 'center',
-      justifyContent: 'center',
-  },  
+      alignItems: 'flex-start',
+      justifyContent: 'center'
+  }, 
+  headerText: {
+    color: '#ccc',
+    fontSize: 18,
+    marginTop: 40,
+    width:'100%'
+  },
+  imageHolder: {
+    height: 220
+  },
+  activityIndicator: {  
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 80,
+    width:'100%'
+ },
+ imageData: {
+   color: '#ccc',
+   fontSize: 16,
+   marginBottom: 20,
+   width:'100%'
+ }
 });
